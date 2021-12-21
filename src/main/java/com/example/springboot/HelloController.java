1220fe6a2b4c2499cec531f7f43b89943f3ce09e
yafernandes <alexandrino.fernandes@gmail.com>
2021-12-21 13:31:23 -0500

    Added Exception

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
-	public String index() {
+	public String index(@RequestParam(required = false) String throwException) {
+		if (throwException != null) {
+			throw new RuntimeException(throwException);
+		}
 		return "Greetings from Spring Boot!";
 	}
 
